0e2a87ccf9ddf448c130742acaf161e6a1e58dae
Grant G <ggp493@gmail.com>
2012-09-19 21:22:36 -0400

    gameEngine loop progress

--- gameEngine.py
+++ gameEngine.py
@@ -5,21 +5,29 @@ from pygame.compat import geterror
 
 #----------------Initialize Game ------------------------------#
 def gameInit():
+	
 	#Load Stuff
+	print "Loading..."
+
+	#Clear Menu Objects
 
 	#Run game loop once done loading
 	gameLoop()
 
 #-----------------------Main loop------------------------------#
 def gameLoop():
+	probe=0
 	running=True
 	e = pygame.event.wait()
 	while (running):
-
-		#Print Initial Map
+		
+		#Print Level
 
 
 		#Check for User Input
 
 
-		#
+		#Update Game
+		running=False
+		pygame.quit()
+		sys.exit()
