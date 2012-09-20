6f909ef24a22d378d99463fccdbea15f5ed0fb48
Grant G <ggp493@gmail.com>
2012-09-19 20:50:29 -0400

    main update

--- gameEngine.py
+++ gameEngine.py
@@ -3,21 +3,6 @@ import pygame,sys,ConfigParser
 from pygame.locals import *   #constants defined here
 from pygame.compat import geterror
 
-if not pygame.font:
-    print ('Warning, fonts disabled')
-if not pygame.mixer:
-    print ('Warning, sound disabled')
-
-#----------------------Read in config file------------------------
-parser = ConfigParser.SafeConfigParser()
-parser.read('config')
-
-fpsCap = parser.get('video', 'fpsCap')
-
-
-#---------------------- Set Window Settings ----------------------
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Nomad Wars - By Grant G. and Brian H.")
 
 #-----------------------Main loop------------------------------#
+def gameLoop
--- main.py
+++ main.py
@@ -1,5 +1,72 @@
 import sprites,menus,levels
+import gameEngine
 import pygame,sys,ConfigParser
+
+if not pygame.font:
+    print ('Warning, fonts disabled')
+if not pygame.mixer:
+    print ('Warning, sound disabled')
+
+#----------------------Read in config file------------------------
+parser = ConfigParser.SafeConfigParser()
+parser.read('config')
+
+fpsCap = parser.get('video', 'fpsCap')
+
+
+#---------------------- Set Window Settings ----------------------
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Nomad Wars - By Grant G. and Brian H.")
+
+#Main menu
+import sys, pygame, random
+import gameEngine
+from menu import *
+def main(screen):
+    print "main menu loaded"
+    menu = cMenu(50, 50, 20, 5, 'vertical', 100, screen,
+               [('Start Game', 1, None),
+                ('Load Game',  2, None),
+                ('Options',    3, None),
+                ('Exit',       4, None)])
+    menu.set_center(True, True)
+    menu.set_alignment('center', 'center')
+    state = 0
+    prev_state = 1
+    rect_list = []
+    pygame.event.set_blocked(pygame.MOUSEMOTION)
+
+
+    while True:
+        if prev_state != state:
+            pygame.event.post(pygame.event.Event(EVENT_CHANGE_STATE, key = 0))
+            prev_state = state
+
+        e = pygame.event.wait()
+        if e.type == pygame.KEYDOWN or e.type == EVENT_CHANGE_STATE:
+            if state == 0:
+                rect_list, state = menu.update(e, state)
+            elif state == 1:
+                print 'Start Game!'
+
+                state = 0
+            elif state == 2:
+                print 'Load Game!'
+                state = 0
+            elif state == 3:
+                print 'Options!'
+                state = 0
+            else:
+                print 'Exit!'
+                pygame.quit()
+                sys.exit()
+        if e.type == pygame.QUIT:
+            print 'test'
+            pygame.quit()
+            sys.exit()
+
+        pygame.display.update(rect_list)
 
 #---- Call Main Menu ------#
 menus.mainMenu.main(screen)
